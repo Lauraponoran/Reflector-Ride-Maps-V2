296b2c4ea479ecee306bd954c1c70457cd38d84c
Lauraponoran <lauraponoran7@gmail.com>
2025-11-02 15:49:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ export const CONFIG = {
   MAP_STYLE: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
   MAP_CENTER: [4.9041, 52.3676], // Amsterdam
   MAP_ZOOM: 13,
-  PMTILES_URL: '/Reflector-Ride-Maps-V2/trips.pmtiles',
-  DATA_URL: '/Reflector-Ride-Maps-V2/sensor_data'
+  PMTILES_URL: '/Reflector-Ride-Maps-V2/trips.pmtiles', // This path looks correct
+  DATA_URL: '/Reflector-Ride-Maps-V2/sensor_data' // Updated to sensor_data
 };
 // Also make available globally for non-module scripts
 window.CONFIG = CONFIG;
